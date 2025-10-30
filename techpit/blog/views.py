fab502f6cee23ca7a9416bee0fc2b53fc88e95b7
AkimJemi <rlawoals623@gmail.com>
2025-10-30 22:55:22 +0900

    20251030 2章 済み

--- techpit/blog/views.py
+++ techpit/blog/views.py
@@ -9,5 +9,17 @@ from django.views.generic import TemplateView
 
 def index(request):
     # TOP画面を表示する関数
-    return render(request, 'index.html')
+    print("index関数を使ってTOP画面を表示します！")  # 追加するコード
+    return render(request, 'blog/index.html')
 # ----ここまで追加----
+
+
+class IndexView(TemplateView):
+    # OP画面を表示するクラス
+    print("IndexView関数を使ってTOP画面を表示します！")  # 追加するコード
+    template_name = 'blog/index.html'  # 追加するコード
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        print("IndexViewを使ってTOP画面を表示します！")
+        return self.render_to_response(context)
